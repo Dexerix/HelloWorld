d004cf275699656c9d3a8c3dfeaf6036403d61b4
Dorian <dorian.luyet@netplus.ch>
2024-07-24 18:50:47 +0200

    Useless elements removed

--- HelloWorld/HelloWorld_V1.py
+++ HelloWorld/HelloWorld_V1.py
@@ -1,7 +1,2 @@
-import os
-os.system('cls')
 def HelloWorld(print):
     print=print("Hello World")
-
-
-HelloWorld(print)
--- HelloWorld/HelloWorld_V2.py
+++ HelloWorld/HelloWorld_V2.py
@@ -1,10 +1,5 @@
-import os
-os.system('cls')
 def HelloWorld(entry:str):
     if entry == 'print':
         print('Hello World')
     else:
         print(entry)
-
-
-HelloWorld("print")
