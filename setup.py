8063aa7d87e4b995effea70d5bba4704f699caea
Dorian <dorian.luyet@netplus.ch>
2024-07-24 15:21:26 +0200

    ¨¨

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name='HelloWorld',
     url='https://github.com/Dexerix/HelloWorld',
     author='Dorian Luyet',
-    packages=['package'],
-    package_dir={'HelloWorld':'package'},
+    packages=['HelloWorld'],
+    package_dir={'HelloWorld':'HelloWorld'},
     version='0.6',
     license='MIT',
     description='a library that offers fun and innovative new ways to print and do Hello Worlds'
